e6ae41012f6ff7c33e79136c6f0271c2d494e783
Garegin.Badalov <garegin.badalov@simbirsoft.com>
2021-05-15 21:23:33 +0300

    homework1

---
+++ Lesson_1/task_2.py
@@ -0,0 +1,8 @@
+import requests
+import json
+import cnf
+
+user = requests.get('https://badalovgm.testrail.io/index.php?/api/v2/get_user/1', auth=(cnf.login, cnf.password)).json()
+
+with open('api_auth.json', 'w', encoding='UTF-8') as fp:
+    json.dump(user, fp,  ensure_ascii=False)
